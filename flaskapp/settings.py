567c215a416f95024beb7efffd8083e43e9c39bd
doubledead <jaa2015@live.com>
2016-09-13 15:18:31 -0500

    Updates for email registration testing.

--- flaskapp/settings.py
+++ flaskapp/settings.py
@@ -10,13 +10,13 @@ CACHE_TYPE = 'simple'
 # SQLite Connection
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 # PostgreSQL Connection
-SQLALCHEMY_DATABASE_URI = 'postgresql://puser:Password1@localhost/devdb1'
-# SQLALCHEMY_DATABASE_URI = 'postgres://nljweakzhspxaa:Cl3n7ipIY1AvTk1MLyslOfZgLz@ec2-54-235-111-59.compute-1.amazonaws.com:5432/dc4t9bjie4nrtc'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://puser:Password1@localhost/devdb1'
+SQLALCHEMY_DATABASE_URI = 'postgres://nljweakzhspxaa:Cl3n7ipIY1AvTk1MLyslOfZgLz@ec2-54-235-111-59.compute-1.amazonaws.com:5432/dc4t9bjie4nrtc'
 SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39a'
 
 # Flask-Mail
 # Required for Flask-Security registration to function properly
-MAIL_DEFAULT_SENDER = 'info@flaskapp.us'
+MAIL_DEFAULT_SENDER = 'service@loveschaos.com'
 MAIL_SERVER = 'smtp.mailgun.org'
 MAIL_PORT = 25
 MAIL_USE_TLS = True
@@ -24,7 +24,8 @@ MAIL_USERNAME = 'postmaster@app612f53d0a96248fdab12a8ad2f5cc871.mailgun.org'
 MAIL_PASSWORD = '9115f88eef5f0426f65301afc351d97e'
 
 # Flask-Security
-SECURITY_CONFIRMABLE = False
+# SECURITY_CONFIRMABLE = False
+SECURITY_CONFIRMABLE = True
 SECURITY_CHANGEABLE = True
 SECURITY_REGISTERABLE = True
 SECURITY_POST_CHANGE_VIEW = '/user'
@@ -32,5 +33,6 @@ SECURITY_POST_LOGIN_VIEW = '/main'
 SECURITY_POST_LOGOUT_VIEW = '/'
 SECURITY_SEND_PASSWORD_CHANGE_EMAIL = False # Prod change
 SECURITY_POST_REGISTER_VIEW = '/main'
-SECURITY_SEND_REGISTER_EMAIL = False
+# SECURITY_SEND_REGISTER_EMAIL = False
+SECURITY_SEND_REGISTER_EMAIL = True
 SECURITY_TRACKABLE = True
