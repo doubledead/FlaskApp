87e4fd15f2c8f8b35ee6888698ba3aa7ab2ad493
doubledead <jaa2015@live.com>
2016-11-15 22:32:29 -0600

    BlockingScheduler test worked. Trying with flaskapp.

--- scheduledjobs.py
+++ scheduledjobs.py
@@ -1,8 +1,8 @@
 
-# from flaskapp import app
-# from flaskapp.core import db
-# from flaskapp.events.models import Event
-# from datetime import datetime, date
+from flaskapp import app
+from flaskapp.core import db
+from flaskapp.events.models import Event
+from datetime import datetime, date
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -12,19 +12,19 @@ sched = BlockingScheduler()
 
 def event_status_check():
     # Check for events with status_id 100, active status.
-    # events = Event.query.filter_by(status_id=100).all()
+    events = Event.query.filter_by(status_id=100).all()
 
-    # for event in events:
-    #     # If the event end_date has expired, change its status_id
-    #     # to 400, completed status.
-    #     if event.end_date <= datetime.utcnow():
-    #         event.status_id = 400
-    #
-    #         db.session.add(event)
+    for event in events:
+        # If the event end_date has expired, change its status_id
+        # to 400, completed status.
+        if event.end_date <= datetime.utcnow():
+            event.status_id = 400
+
+            db.session.add(event)
 
 
     # Commit the db session back.
-    # db.session.commit()
+    db.session.commit()
     print('This job runs every 30 seconds.')
 
 sched.add_job(event_status_check, 'interval', seconds=30)
