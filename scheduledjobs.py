edd8ae3835609d07e31c2afd6f0617c6cd9d269c
doubledead <jaa2015@live.com>
2016-11-16 02:20:43 -0600

    More app.app_context testing.

--- scheduledjobs.py
+++ scheduledjobs.py
@@ -12,49 +12,30 @@ logging.basicConfig(filename='jobs.log',level=logging.DEBUG)
 
 # sched = BackgroundScheduler()
 
-# sched = BlockingScheduler()
+sched = BlockingScheduler()
 
-# def event_status_check():
-#     # Check for events with status_id 100, active status.
-#     events = Event.query.filter_by(status_id=100).all()
-#
-#     for event in events:
-#         # If the event end_date has expired, change its status_id
-#         # to 400, completed status.
-#         if event.end_date <= datetime.utcnow():
-#             event.status_id = 400
-#
-#             db.session.add(event)
-#
-#
-#     # Commit the db session back.
-#     db.session.commit()
-#     print('This job runs every 30 seconds.')
+def event_status_check():
+    # Check for events with status_id 100, active status.
+    events = Event.query.filter_by(status_id=100).all()
+
+    for event in events:
+        # If the event end_date has expired, change its status_id
+        # to 400, completed status.
+        if event.end_date <= datetime.utcnow():
+            event.status_id = 400
+
+            db.session.add(event)
+
+
+    # Commit the db session back.
+    db.session.commit()
+    print('This job runs every 30 seconds.')
 
 # sched.add_job(event_status_check, 'interval', seconds=30)
 
 # sched.start()
 
 with app.app_context():
-    def event_status_check():
-        # Check for events with status_id 100, active status.
-        events = Event.query.filter_by(status_id=100).all()
-
-        for event in events:
-            # If the event end_date has expired, change its status_id
-            # to 400, completed status.
-            if event.end_date <= datetime.utcnow():
-                event.status_id = 400
-
-                db.session.add(event)
-
-
-        # Commit the db session back.
-        db.session.commit()
-        print('This job runs every 30 seconds.')
-
-    sched = BlockingScheduler()
-
     sched.add_job(event_status_check, 'interval', seconds=30)
 
     sched.start()
