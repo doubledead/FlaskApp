61dbb01b657c4ead25f86f79501d9b26dd0d114c
doubledead <jaa2015@live.com>
2016-11-15 22:16:54 -0600

    Cleaned up scheduledjobs.py testing file.

--- scheduledjobs.py
+++ scheduledjobs.py
@@ -1,79 +1,30 @@
 
-from flaskapp import app
-from flaskapp.core import db
-from flaskapp.events.models import Event
-from datetime import datetime, date
+# from flaskapp import app
+# from flaskapp.core import db
+# from flaskapp.events.models import Event
+# from datetime import datetime, date
 
-# from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# scheduler = BackgroundScheduler()
+sched = BackgroundScheduler()
 
-# def event_status_check():
-#     Check for events with status_id 100, active status.
-#     events = Event.query.filter_by(status_id=100).all()
-#
-#     for event in events:
-#         # If the event end_date has expired, change its status_id
-#         # to 400, completed status.
-#         if event.end_date <= datetime.utcnow():
-#             event.status_id = 400
-#
-#             db.session.add(event)
-#
-#
-#     # Commit the db session back.
-#     db.session.commit()
-#     print('This job runs every 30 seconds.')
-#
-# scheduler.add_job(event_status_check, 'interval', seconds=30)
-#
-# scheduler.start()
+def event_status_check():
+    # Check for events with status_id 100, active status.
+    # events = Event.query.filter_by(status_id=100).all()
 
-# from flask import Flask
-from flask_apscheduler import APScheduler
+    # for event in events:
+    #     # If the event end_date has expired, change its status_id
+    #     # to 400, completed status.
+    #     if event.end_date <= datetime.utcnow():
+    #         event.status_id = 400
+    #
+    #         db.session.add(event)
 
 
-# class Config(object):
-#     JOBS = [
-#         {
-#             'id': 'job1',
-#             'func': '__main__:job1',
-#             'args': (),
-#             'trigger': 'interval',
-#             'seconds': 30
-#         }
-#     ]
-#
-#     SCHEDULER_VIEWS_ENABLED = True
-
-
-def job1():
+    # Commit the db session back.
+    # db.session.commit()
     print('This job runs every 30 seconds.')
 
-# app = Flask(__name__)
-# app.config.from_object(Config())
-# app.debug = True
+sched.add_job(event_status_check, 'interval', seconds=30)
 
-# scheduler = APScheduler()
-# scheduler.init_app(app)
-# scheduler.start()
-
-# app.run()
-
-scheduler = APScheduler()
-scheduler.init_app(app)
-
-JOBS = [
-    {
-        'id': 'job1',
-        'func': '__main__:job1',
-        'args': (),
-        'trigger': 'interval',
-        'seconds': 30
-    }
-]
-
-SCHEDULER_VIEWS_ENABLED = True
-
-if __name__ == "__main__":
-    scheduler.start()
+sched.start()
