c7f5ecdc660e8a50736fbe876a2793f1c7ea4bbf
doubledead <jaa2015@live.com>
2017-08-14 18:42:13 -0500

    Updated settings with dev DB credentials and email registration testing.

--- flaskapp/settings.py
+++ flaskapp/settings.py
@@ -11,9 +11,11 @@ CACHE_TYPE = 'simple'
 ## SQLite Connection
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 ## PostgreSQL local connection
-SQLALCHEMY_DATABASE_URI = 'postgresql://puser:Password1@localhost/devdb1'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://puser:Password1@localhost/devdb1'
 ## Heroku flaskapp-pro PostgreSQL database
 # SQLALCHEMY_DATABASE_URI = 'postgres://henizvlbiygjoa:Ze4ZM7v9ApX2CK0HFQAi8EpdX-@ec2-54-235-125-172.compute-1.amazonaws.com:5432/dfgb1crsk1t3d9'
+# Heroku Dev DB
+SQLALCHEMY_DATABASE_URI = 'postgres://nljweakzhspxaa:Cl3n7ipIY1AvTk1MLyslOfZgLz@ec2-54-235-111-59.compute-1.amazonaws.com:5432/dc4t9bjie4nrtc'
 SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39a'
 
 # Flask-Mail
@@ -25,12 +27,13 @@ MAIL_USE_TLS = True
 # Sandbox domain credentials
 # MAIL_USERNAME = 'postmaster@app612f53d0a96248fdab12a8ad2f5cc871.mailgun.org'
 # MAIL_PASSWORD = '9115f88eef5f0426f65301afc351d97e'
+# Personal Media Template email configs
 MAIL_USERNAME = 'postmaster@mg.loveschaos.com'
 MAIL_PASSWORD = '05f20eb390515bce00f8d19d7fb5bbd5'
 
 # Flask-Security
-SECURITY_CONFIRMABLE = False
-# SECURITY_CONFIRMABLE = True
+# SECURITY_CONFIRMABLE = False
+SECURITY_CONFIRMABLE = True
 SECURITY_CHANGEABLE = True
 SECURITY_REGISTERABLE = True
 SECURITY_POST_CHANGE_VIEW = '/user'
