6b3a1e64a4ff3f120862081bda58af8d0a443c11
doubledead <jaa2015@live.com>
2017-10-13 20:00:57 -0500

    Upgraded Python Flask packages.

--- flaskapp/cache.py
+++ flaskapp/cache.py
@@ -1,4 +1,4 @@
-from flask_cache import Cache
+# from flask_cache import Cache
 
 
-cache = Cache()
+# cache = Cache()
--- flaskapp/core.py
+++ flaskapp/core.py
@@ -6,7 +6,7 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security
 from flask_marshmallow import Marshmallow
-from flask.ext.moment import Moment
+from flask_moment import Moment
 from flask_apscheduler import APScheduler
 
 #: Flask-SQLAlchemy extension instance
