c2d4f9236e70448002f5842df09f684aba966206
doubledead <jaa2015@live.com>
2018-05-10 15:38:05 -0500

    Settings updates for Pro settings

--- flaskapp/settings.py
+++ flaskapp/settings.py
@@ -6,8 +6,9 @@ TESTING = False
 
 CACHE_TYPE = 'simple'
 
-## Heroku flaskapp-dev
-SQLALCHEMY_DATABASE_URI = 'postgres://aghyweatfnlblv:d37447d434c2f322f09988076a8ac4d9e3a9c6e4aa6cebfcce963c9d2d803b50@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d6sthrt9kr6aal'
+## Heroku flaskapp-pro
+SQLALCHEMY_DATABASE_URI = 'postgres://henizvlbiygjoa:Ze4ZM7v9ApX2CK0HFQAi8EpdX-@ec2-54-235-125-172.compute-1.amazonaws.com:5432/dfgb1crsk1t3d9'
+# SQLALCHEMY_DATABASE_URI = 'postgres://aghyweatfnlblv:d37447d434c2f322f09988076a8ac4d9e3a9c6e4aa6cebfcce963c9d2d803b50@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d6sthrt9kr6aal'
 SECRET_KEY = 'a9eec0e0-23b7-4788-9a92-318347b9a39a'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
